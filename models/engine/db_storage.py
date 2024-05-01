5dbde892c3165e4dd4ca6174c10cbb7952a4ddb7
Lexzino <alexudeze001@gmail.com>
2024-05-01 08:06:44 -0700

    updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-Contains the class DBStorage
-"""
+"""Contains the class DBStorage"""
 
 import models
 from models.amenity import Amenity
@@ -12,9 +10,9 @@ from models.review import Review
 from models.state import State
 from models.user import User
 from os import getenv
-import sqlalchemy # type: ignore
-from sqlalchemy import create_engine # type: ignore
-from sqlalchemy.orm import scoped_session, sessionmaker # type: ignore
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 classes = {"Amenity": Amenity, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
